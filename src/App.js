08b9d5c58146a0a3e410c3224406ee9df9b5fe10
Nate Hamshar <williamhamshar@gmail.com>
2022-06-16 07:56:50 -0400

    axios get is working and functioning properly

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from "axios";
 
 function App() {
+
+  const [data, setData] = useState("");
+  const [song, setSong] = useState("oi bruv");
+
+
+  const getResult = () => {
+    axios.get()
+      .then(res => {
+        return res.data
+      })
+      .then((data) => {
+        setData(data)
+      })
+      .catch(error => console.log(error.message))
+  };
+ 
+  useEffect(() => {
+    if(!data){
+      return;
+    } else{
+      setSong(data.lyrics)
+    }
+    }, [data]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className="getLyrics" onClick={() => getResult()}>get lyrics</button>
+      <div className='songLyrics'>{song}</div>
     </div>
   );
 }
